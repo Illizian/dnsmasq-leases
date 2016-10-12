05bf6774f65202ef00e3a4a936dd1c96805b9275
Alex Scotton <alex@alexscotton.com>
2016-10-12 12:04:22 +0100

    Fix #1 by ignoring comment & empty lines

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const matcher = /(\d{10})\s((?:[a-zA-Z0-9]{2}[:-]){5}[a-zA-Z0-9]{2})\s((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s(\S+)\s(\S+)/;
+const comment = /^\s*[//|#]/;
 
 module.exports = function leases(str) {
-  return str.split('\n').map((line, index) => {
-    let result = matcher.exec(line);
-    if (!result) throw new Error(`Unable to parse lease on line ${ index + 1 }`);
-
-    return {
-      timestamp: result[1],
-      mac: result[2],
-      ip: result[3],
-      host: result[4],
-      id: result[5]
-    };
-  });
+  return str.split('\n').map(processLine).filter(Boolean);
 };
+
+function processLine(line, index) {
+  if (!line.trim()) return false;
+  if (comment.test(line)) return false;
+
+  let result = matcher.exec(line);
+  if (!result) throw new Error(`Unable to parse lease on line ${ index + 1 }`);
+
+  return {
+    timestamp: result[1],
+    mac: result[2],
+    ip: result[3],
+    host: result[4],
+    id: result[5]
+  };
+}
